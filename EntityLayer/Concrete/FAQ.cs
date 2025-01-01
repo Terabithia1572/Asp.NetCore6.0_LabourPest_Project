168a4700b7e1d489501a97d44169071a0b12954a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-01 23:17:08 +0300

    FAQ + JobApplication + Product Process Added

---
+++ EntityLayer/Concrete/FAQ.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class FAQ
+    {
+        [Key]
+        public int FAQID { get; set; }
+        public string FAQTitle { get; set; }
+        public string FAQDescription { get; set; }
+        public bool FAQStatus { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/JobApplication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class JobApplication
+    {
+        [Key]
+        public int JobApplicationID { get; set; }
+        public string JobApplicationName { get; set; }
+        public string JobApplicationSurName { get; set; }
+        public string JobApplicationNumber { get; set; }
+        public string JobApplicationMail { get; set; }
+        public string JobApplicationCity { get; set; }
+        public string JobApplicationCV { get; set; }
+        public string JobApplicationAboutYou { get; set; }
+        public string JobApplicationStatus { get; set; }
+    }
+}
