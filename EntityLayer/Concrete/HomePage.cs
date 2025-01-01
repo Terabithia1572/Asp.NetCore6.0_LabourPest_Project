c7eda8e4124cb7e509ecf7149e155a7d1a495d19
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-01 23:02:56 +0300

    Concrete Folder Added + HomePage+Services + WhoWeUs Database Process Added

---
+++ EntityLayer/Concrete/HomePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class HomePage
+    {
+        [Key]
+        public int HomePageID { get; set; }
+        public string HomePageDescription1 { get; set; }
+        public string HomePageDescription2 { get; set; }
+        public string HomePageSubDescription { get; set; }
+        public string HomePageClass { get; set; }
+        public bool HomePageStatus { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/WhoWeUs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class WhoWeUs
+    {
+        [Key]
+        public int WhoWeUsID { get; set; }
+        public string WhoWeUsTitle1 { get; set; }
+        public string WhoWeUsTitle2 { get; set; }
+        public string WhoWeUsDescription1 { get; set; }
+        public string WhoWeUsDescription2 { get; set; }
+        public string WhoWeUsImageURL { get; set; }
+        public bool WhoWeUsStatus { get; set; }
+    }
+}
