36cf0bb3913db821f5d7c7dbe287ec2844cb5b4a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-01 18:19:13 +0300

    Add project files.

---
+++ Asp.NetCore6.0_LabourPest_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetCore6._0_LabourPest_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
