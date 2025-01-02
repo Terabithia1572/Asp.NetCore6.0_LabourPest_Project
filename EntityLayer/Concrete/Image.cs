38832aa836b08d5230256ee532699a19b10f9067
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-02 20:36:39 +0300

    Brands + Image Process Added

---
+++ EntityLayer/Concrete/Image.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Image
+    {
+        [Key]
+        public int ImageID { get; set; }
+        public string ImageUpload { get; set; }
+        public bool ImageStatus { get; set; }
+    }
+}
