77642a809a9c9fbd1c899fe5f341c1594de51fa2
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-02 21:25:31 +0300

    Abstract Folder Added + Abstract Process Added + Generic Repository Process Added

--- DataAccessLayer/Repositories/GenericRepository.cs
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IGenericDal<T> where T : class
     {
+        Context context = new();
+        public void Delete(T t)
+        {
+            context.Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public List<T> GetListAll(Expression<Func<T, bool>> filter)
+        {
+            return context.Set<T>().Where(filter).ToList();
+        }
+
+        public void Insert(T t)
+        {
+            context.Add(t);
+            context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            context.Update(t);
+            context.SaveChanges();
+        }
     }
 }
