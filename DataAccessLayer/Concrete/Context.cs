222e32f8276725be78b860dde099d3924e239f6f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-02 21:06:36 +0300

    Database Process Added + Migration Added

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,37 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=LabourPestDB;integrated security=true;");
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Brands> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<FAQ> FAQs { get; set; }
+        public DbSet<HomePage> HomePages { get; set; }
+        public DbSet<Image> Images { get; set; }      
+        public DbSet<JobApplication> JobApplications { get; set; }
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<Mail> Mails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Services> Services { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<WhoWeUs> WhoWeUs { get; set; }
+       
+        
+    }
+}
---
+++ DataAccessLayer/Migrations/20250102180425_mig_First_LabourPest_Database.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_First_LabourPest_Database : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    AdminID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShortDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.AdminID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    BrandsID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BrandsImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BrandsStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.BrandsID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    CommentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CommentUserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommentTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommentContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CommentStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.CommentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactUserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactSubject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactMessage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ContactStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeSurName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeTwitter = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeFacebook = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeInstagram = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeLinkedin = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FAQs",
+                columns: table => new
+                {
+                    FAQID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FAQTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FAQDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FAQStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FAQs", x => x.FAQID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HomePages",
+                columns: table => new
+                {
+                    HomePageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HomePageDescription1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HomePageDescription2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HomePageSubDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HomePageClass = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HomePageStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HomePages", x => x.HomePageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    ImageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageUpload = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.ImageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobApplications",
+                columns: table => new
+                {
+                    JobApplicationID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JobApplicationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobApplicationSurName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobApplicationNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobApplicationMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobApplicationCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobApplicationCV = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobApplicationAboutYou = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JobApplicationStatus = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobApplications", x => x.JobApplicationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Success = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mails",
+                columns: table => new
+                {
+                    MailID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MailName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MailSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MailTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MailContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MailDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mails", x => x.MailID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductImageURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    ServicesID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServicesTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ServicesDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ServicesImageURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ServicesStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.ServicesID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subscribes",
+                columns: table => new
+                {
+                    SubscribeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubscribeMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubscribeStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscribes", x => x.SubscribeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WhoWeUs",
+                columns: table => new
+                {
+                    WhoWeUsID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WhoWeUsTitle1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WhoWeUsTitle2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WhoWeUsDescription1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WhoWeUsDescription2 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WhoWeUsImageURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WhoWeUsStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WhoWeUs", x => x.WhoWeUsID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "Brands");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "FAQs");
+
+            migrationBuilder.DropTable(
+                name: "HomePages");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "JobApplications");
+
+            migrationBuilder.DropTable(
+                name: "Logs");
+
+            migrationBuilder.DropTable(
+                name: "Mails");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "Subscribes");
+
+            migrationBuilder.DropTable(
+                name: "WhoWeUs");
+        }
+    }
+}
