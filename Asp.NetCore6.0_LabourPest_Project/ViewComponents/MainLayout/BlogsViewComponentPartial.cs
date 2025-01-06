641e68a76fee4db4bd2c04afa2b26cf1d4758e07
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-06 21:16:18 +0300

    Blogs View Component Process Added

---
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/BlogsViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.MainLayout
+{
+	public class BlogsViewComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
