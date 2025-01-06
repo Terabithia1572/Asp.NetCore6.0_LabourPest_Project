8d3aa3fc7cc134e4e9e87d8e14d9a23611ae3357
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-06 20:38:29 +0300

    PageHeader + PageSlider + Product View Component Process Added

---
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/ProductViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.MainLayout
+{
+	public class ProductViewComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
