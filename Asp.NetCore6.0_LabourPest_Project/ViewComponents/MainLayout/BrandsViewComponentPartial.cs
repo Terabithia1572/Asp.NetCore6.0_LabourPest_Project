f16e29701a741b1ca89931ed9f94715828afc82e
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-08 22:35:47 +0300

    Brands Page Added + Carousel new Animation Added + new JS files Added

--- Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/BrandsViewComponentPartial.cs
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/BrandsViewComponentPartial.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.MainLayout
 {
 	public class BrandsViewComponentPartial:ViewComponent
 	{
+		BrandsManager brandsManager = new BrandsManager(new EfBrandsRepository());
 		public IViewComponentResult Invoke()
 		{
-			return View();
+			var values=brandsManager.GetAll();
+			return View(values);
 		}
 	}
 }
---
+++ Asp.NetCore6.0_LabourPest_Project/wwwroot/canabicom/dynamic2/dynamic3.js
@@ -0,0 +1,22 @@
+<script>
+    $(document).ready(function () {
+        $('#partners-carousel').owlCarousel({
+            loop: true, // Sonsuz döngü
+            margin: 20, // Her resim arasındaki boşluk
+            autoplay: true, // Otomatik oynatma
+            autoplayTimeout: 3000, // 3 saniyede bir kaydırma
+            autoplayHoverPause: true, // Fareyle üzerine gelince durdurma
+            responsive: {
+                0: {
+                    items: 2 // Küçük ekranlarda 2 resim
+                },
+                600: {
+                    items: 3 // Orta ekranlarda 3 resim
+                },
+                1000: {
+                    items: 6 // Büyük ekranlarda 6 resim
+                }
+            }
+        });
+    });
+</script>
