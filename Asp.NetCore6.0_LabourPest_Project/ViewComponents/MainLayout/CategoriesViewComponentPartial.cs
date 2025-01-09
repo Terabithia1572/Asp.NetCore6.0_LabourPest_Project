d267766ef54d59a630ec5c106e2bdf024f97febc
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-10 00:17:19 +0300

    Category Database Process Updated + Category Database List Process Added

--- Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/CategoriesViewComponentPartial.cs
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/CategoriesViewComponentPartial.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.MainLayout
 {
 	public class CategoriesViewComponentPartial:ViewComponent
 	{
-		public IViewComponentResult Invoke()
+		CategoryManager categoryManager= new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
 		{
-			return View();
+			var values=categoryManager.GetAll();
+            return View(values);
 		}
 	}
 }
---
+++ DataAccessLayer/Migrations/20250109211628_mig_Category_Images_column_Added.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_Category_Images_column_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryImages",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryImages",
+                table: "Categories");
+        }
+    }
+}
