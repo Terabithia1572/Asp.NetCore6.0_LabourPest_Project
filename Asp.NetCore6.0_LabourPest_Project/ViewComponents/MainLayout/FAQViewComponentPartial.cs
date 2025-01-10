28256e12d5179d173cff0e4e6d8f8a665f58b814
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-10 22:41:07 +0300

    FAQ View Component Prcess Added + FAQ Page Theme Updated

--- Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/FAQViewComponentPartial.cs
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/FAQViewComponentPartial.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.MainLayout
 {
 	public class FAQViewComponentPartial:ViewComponent
 	{
-		public IViewComponentResult Invoke()
+		FAQManager fAQManager = new FAQManager(new EfFAQRepository());
+		
+        public IViewComponentResult Invoke()
 		{
-			return View();
+            var allFAQs = fAQManager.GetAll();
+
+            // Kategorilere göre ayır
+            var accordion01List = allFAQs.Where(f => f.FAQStatus == true).ToList();
+            var accordion02List = allFAQs.Where(f => f.FAQStatus == false).ToList();
+
+            // ViewData veya bir ViewModel kullanarak verileri gönder
+            ViewData["Accordion01"] = accordion01List;
+            ViewData["Accordion02"] = accordion02List;
+
+            
+            return View();
 		}
 	}
 }
