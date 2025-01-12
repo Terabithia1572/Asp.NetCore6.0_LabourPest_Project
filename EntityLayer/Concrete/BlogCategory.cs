5504d3766801f2e5e6f99ea6451b1e96cb496e15
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-12 15:09:33 +0300

    Blog + BlogCategory + Writer Database Tables Added

---
+++ EntityLayer/Concrete/BlogCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class BlogCategory
+    {
+        [Key]
+        public int BlogCategoryID { get; set; }
+        public string BlogCategories { get; set; }
+        public bool BlogCategoryStatus { get; set; }
+    }
+}
