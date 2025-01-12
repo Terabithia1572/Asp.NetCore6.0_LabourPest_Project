f9a61049a711ed2ee8bdfe0f09b77c9ab03dfacb
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-12 17:49:04 +0300

    Blog Controller Updated

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/BlogController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        BlogCategoryManager blogCategoryManager = new BlogCategoryManager(new EfBlogCategoryRepository());
+        Context c = new();
+		private readonly BlogManager _blogManager;
+
+		public BlogController(BlogManager blogManager)
+		{
+			_blogManager = blogManager;
+		}
+		public IActionResult BlogDetails(int id)
+		{
+			var blog = _blogManager.TGetByID(id); // Blog detayını getiren metot
+			if (blog == null)
+			{
+				return NotFound(); // Blog bulunamazsa 404
+			}
+			return View(blog);
+		}
+
+		public IActionResult Index()
+        {
+            var values = blogManager.GetBlogListWithBlogCategory();
+            return View(values);
+        }
+        public IActionResult BlogReadAll(int id)
+        {
+            ViewBag.i = id;
+            var values = blogManager.TGetByID(id);
+            return View(values);
+        }
+    }
+}
