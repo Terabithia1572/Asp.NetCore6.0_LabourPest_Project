7d8c5dc57abf5c1fbf03a093acd597a37855abfb
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-12 15:43:21 +0300

    BlogComment Business Layer Process Added

---
+++ BusinessLayer/Concrete/BlogCommentManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogCommentManager : IBlogCommentService
+    {
+        IBlogCommentDal _blogCommentDal;
+
+        public BlogCommentManager(IBlogCommentDal blogCommentDal)
+        {
+            _blogCommentDal = blogCommentDal;
+        }
+
+        public List<BlogComment> GetAll()
+        {
+            return _blogCommentDal.GetListAll();
+        }
+
+        public void TAdd(BlogComment t)
+        {
+            _blogCommentDal.Insert(t);
+        }
+
+        public void TDelete(BlogComment t)
+        {
+            _blogCommentDal.Delete(t);
+        }
+
+        public BlogComment TGetByID(int id)
+        {
+            return _blogCommentDal.GetByID(id);
+        }
+
+        public void TUpdate(BlogComment t)
+        {
+            _blogCommentDal.Update(t);
+        }
+    }
+}
