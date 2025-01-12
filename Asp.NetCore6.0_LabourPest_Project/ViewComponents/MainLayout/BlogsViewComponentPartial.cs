89a1101f2a38b70dd5a2c80ecc7bebda56946cd6
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-12 17:48:44 +0300

    Main Layout + Program.cs Updated

--- Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/BlogsViewComponentPartial.cs
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/BlogsViewComponentPartial.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.MainLayout
 {
 	public class BlogsViewComponentPartial:ViewComponent
 	{
+		BlogManager blogManager = new BlogManager(new EfBlogRepository());
 		public IViewComponentResult Invoke()
 		{
-			return View();
+			var values = blogManager.GetBlogListWithBlogCategory();
+			return View(values);
 		}
 	}
 }
