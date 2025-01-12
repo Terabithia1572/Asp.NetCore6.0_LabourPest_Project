17176f968b2cbf6c35e77a04cfa8e7f41de7a5a9
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-12 15:11:48 +0300

    Blog + BlogCategory + Writers Migration Process Added

--- DataAccessLayer/Migrations/20250112121109_mig_Blog_BlogCategory_Writer_TablesAdded.Designer.cs
+++ DataAccessLayer/Migrations/20250112121109_mig_Blog_BlogCategory_Writer_TablesAdded.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250112121109_mig_Blog_BlogCategory_Writer_TablesAdded")]
+    partial class mig_Blog_BlogCategory_Writer_TablesAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccessLayer/Migrations/20250112121109_mig_Blog_BlogCategory_Writer_TablesAdded.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_Blog_BlogCategory_Writer_TablesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlogCategories",
+                columns: table => new
+                {
+                    BlogCategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogCategories = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BlogCategoryStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogCategories", x => x.BlogCategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BlogContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BlogImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BlogDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BlogStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Writers",
+                columns: table => new
+                {
+                    WriterID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WriterName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WriterSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WriterAbout = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WriterImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WriterMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WriterPassword = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WriterStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Writers", x => x.WriterID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogCategories");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Writers");
+        }
+    }
+}
