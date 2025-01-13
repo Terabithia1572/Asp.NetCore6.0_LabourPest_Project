6fd80fe11aba25f0eddbd736994b0bceb6ee41b5
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-13 18:27:17 +0300

    Customer Register Controller Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/RegisterCustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+	public class RegisterCustomerController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
