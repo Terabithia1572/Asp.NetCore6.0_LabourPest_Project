5778f7935b3a29d74b0dca80210b7932f08be10b
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-17 23:20:04 +0300

    Customer Dashboard Controller Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/CustomerDashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class CustomerDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
