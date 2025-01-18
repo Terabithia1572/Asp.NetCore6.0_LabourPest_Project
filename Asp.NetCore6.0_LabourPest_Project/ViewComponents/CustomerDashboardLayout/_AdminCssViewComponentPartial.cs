c43bb95e472efed6cbd02e1152e2b43263cad62c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-18 14:02:46 +0300

    Admin Css + Component Partial Page Added

---
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/CustomerDashboardLayout/_AdminCssViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.CustomerDashboardLayout
+{
+    public class _AdminCssViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
