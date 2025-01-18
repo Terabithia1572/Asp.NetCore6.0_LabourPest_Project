2f44a4c85501b84fb5388d3f45da5e20884c52c1
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-18 14:32:51 +0300

    Admin Dashboard Controller Added + Profile Page Process Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/AdminDashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class AdminDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
