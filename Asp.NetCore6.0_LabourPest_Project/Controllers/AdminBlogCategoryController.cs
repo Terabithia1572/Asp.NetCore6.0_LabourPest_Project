0af3bb0efabee39e3fd1e779f44bd47c7c0d5c3d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-18 15:35:23 +0300

    Admin Category Process Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/AdminBlogCategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class AdminBlogCategoryController : Controller
+    {
+        public IActionResult CategoryList()
+        {
+            return View();
+        }
+    }
+}
