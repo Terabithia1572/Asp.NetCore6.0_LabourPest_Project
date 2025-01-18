6df0d92dbeecbff8447a1e5b40397816413dfadd
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-18 22:56:05 +0300

    Admin Blog List Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/AdminBlogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class AdminBlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BlogList()
+        {
+            return View();
+        }
+    }
+}
