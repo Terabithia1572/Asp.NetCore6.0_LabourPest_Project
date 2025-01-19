91344bbbcb567beffb368b3c4143b04029edd41c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-19 14:17:08 +0300

    RegisterCustomer Process Added

--- Asp.NetCore6.0_LabourPest_Project/Controllers/RegisterCustomerController.cs
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/RegisterCustomerController.cs
@@ -1,16 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore6._0_LabourPest_Project.Controllers
 {
-	public class RegisterCustomerController : Controller
+	
+    public class RegisterCustomerController : Controller
 	{
-		public IActionResult Index()
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+        public IActionResult Index()
 		{
 			return View();
 		}
 		public IActionResult CustomerList()
 		{
-			return View();
+			var values = writerManager.GetAll();
+            return View(values);
 		}
-	}
+		[HttpGet]
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCustomer(Writer writer)
+        {
+            writer.WriterStatus = true;
+            writerManager.TAdd(writer);
+            return RedirectToAction("CustomerList", "RegisterCustomer");
+        }
+    }
 }
