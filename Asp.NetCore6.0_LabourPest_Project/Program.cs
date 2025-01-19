cb680d3a5aa1b232fb77c7d6febc03860510c56e
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-19 15:06:58 +0300

    Customer Image Process Added

--- Asp.NetCore6.0_LabourPest_Project/Program.cs
+++ Asp.NetCore6.0_LabourPest_Project/Program.cs
@@ -27,8 +27,13 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllers(); // UploadImage için gerekli
+});
+
 
 app.Run();
