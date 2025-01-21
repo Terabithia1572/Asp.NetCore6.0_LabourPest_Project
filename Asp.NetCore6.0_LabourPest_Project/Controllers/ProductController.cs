de2fb80ba78d94b8be8bda31573982cc49c625e5
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-21 21:36:47 +0300

    Product Controller Updated CRUD Process Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/ProductController.cs
@@ -0,0 +1,102 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+	public class ProductController : Controller
+	{
+		ProductManager productManager = new ProductManager(new EfProductRepository());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult ProductList()
+		{
+			var values=productManager.GetListWithProductCategory();
+			return View(values);
+		}
+
+		[HttpGet]
+        public IActionResult AddProduct()
+        {
+            List<SelectListItem> categoryValues = (from x in categoryManager.GetAll()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID.ToString()
+                                                   }).ToList();
+            ViewBag.cv = categoryValues;
+            return View();
+        }
+		[HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            product.ProductStatus = true;
+            productManager.TAdd(product);
+            return RedirectToAction("ProductList", "Product");
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            var values = productManager.TGetByID(id);
+            productManager.TDelete(values);
+            return RedirectToAction("ProductList", "Product");
+        }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            List<SelectListItem> categoryValues = (from x in categoryManager.GetAll()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID.ToString()
+                                                   }).ToList();
+            ViewBag.cv = categoryValues;
+           
+            var values = productManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            product.ProductStatus = true;
+            productManager.TUpdate(product);
+            return RedirectToAction("ProductList", "Product");
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                // Klasör yolunu tanımla
+                string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "labourpestcustomer", "productImage");
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                // Dosya ismini oluştur
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                // Dosya yolunu oluştur
+                string filePath = Path.Combine(folderPath, fileName);
+
+                // Dosyayı kaydet
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                // Dosya yolunu döndür
+                string relativePath = $"/labourpestcustomer/productImage/{fileName}";
+                return Json(new { filePath = relativePath });
+            }
+
+            return BadRequest("Dosya yüklenemedi!");
+        }
+    }
+}
