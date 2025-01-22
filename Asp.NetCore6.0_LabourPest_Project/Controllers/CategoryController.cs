0fde3aa61dfbdb8a8b779a20b2148adfde9bda65
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-22 21:24:12 +0300

    Category Controller Process Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+      
+        public IActionResult CategoryList()
+        {
+            var values = categoryManager.GetAll();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            category.CategoryStatus = true;
+            categoryManager.TAdd(category);
+            return RedirectToAction("CategoryList", "Category");
+        }
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var values = categoryManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateCategory(Category Category)
+        {
+            Category.CategoryStatus = true;
+            categoryManager.TUpdate(Category);
+            return RedirectToAction("CategoryList", "Category");
+        }
+        public IActionResult DeleteCategory(int id)
+        {
+            var values = categoryManager.TGetByID(id);
+            categoryManager.TDelete(values);
+            return RedirectToAction("CategoryList", "Category");
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                // Klasör yolunu tanımla
+                string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "labourpestcustomer", "categoryImage");
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                // Dosya ismini oluştur
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                // Dosya yolunu oluştur
+                string filePath = Path.Combine(folderPath, fileName);
+
+                // Dosyayı kaydet
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                // Dosya yolunu döndür
+                string relativePath = $"/labourpestcustomer/categoryImage/{fileName}";
+                return Json(new { filePath = relativePath });
+            }
+
+            return BadRequest("Dosya yüklenemedi!");
+        }
+    }
+}
