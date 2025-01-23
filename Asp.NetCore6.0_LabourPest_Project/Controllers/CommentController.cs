ce9a5a5375bb2b0d0f4ced874e0caa4959d5af86
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-23 20:32:44 +0300

    Comment Controller Process Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/CommentController.cs
@@ -0,0 +1,87 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CommentList()
+        {
+            var values = commentManager.GetAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddComment()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddComment(Comment comment)
+        {
+            comment.CommentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            comment.CommentStatus = true;
+            commentManager.TAdd(comment);
+            return RedirectToAction("CommentList","Comment");
+        }
+        public IActionResult DeleteComment(int id)
+        {
+            var commentValue = commentManager.TGetByID(id);
+            commentManager.TDelete(commentValue);
+            return RedirectToAction("CommentList", "Comment");
+        }
+        [HttpGet]
+        public IActionResult UpdateComment(int id)
+        {
+            var commentValue = commentManager.TGetByID(id);
+            return View(commentValue);
+        }
+        [HttpPost]
+        public IActionResult UpdateComment(Comment comment)
+        {
+            comment.CommentStatus = true;
+            comment.CommentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            commentManager.TUpdate(comment);
+            return RedirectToAction("CommentList", "Comment");
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                // Klasör yolunu tanımla
+                string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "labourpestcustomer", "commentImage");
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                // Dosya ismini oluştur
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                // Dosya yolunu oluştur
+                string filePath = Path.Combine(folderPath, fileName);
+
+                // Dosyayı kaydet
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                // Dosya yolunu döndür
+                string relativePath = $"/labourpestcustomer/commentImage/{fileName}";
+                return Json(new { filePath = relativePath });
+            }
+
+            return BadRequest("Dosya yüklenemedi!");
+        }
+
+    }
+}
