62986ec02942a0420adf4f236823dcd27d073a72
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-24 22:12:56 +0300

    Job Application Process Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/JobApplicationController.cs
@@ -0,0 +1,21 @@
+﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class JobApplicationController : Controller
+    {
+        JobApplicationManager jobApplicationManager = new JobApplicationManager(new EfJobApplicationRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult JobApplicationList()
+        {
+            var values = jobApplicationManager.GetAll();
+            return View(values);
+        }
+    }
+}
