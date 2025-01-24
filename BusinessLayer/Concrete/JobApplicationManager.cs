ebc9bc54c541744585171b5e6b3c36b95036f2ec
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-24 22:14:40 +0300

    Job Application Business Process Updated

---
+++ BusinessLayer/Concrete/JobApplicationManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class JobApplicationManager : IJobApplicationService
+    {
+        IJobApplicationDal _jobApplicationDal;
+
+        public JobApplicationManager(IJobApplicationDal jobApplicationDal)
+        {
+            _jobApplicationDal = jobApplicationDal;
+        }
+
+        public List<JobApplication> GetAll()
+        {
+           return _jobApplicationDal.GetListAll();
+        }
+
+        public void TAdd(JobApplication t)
+        {
+            _jobApplicationDal.Insert(t);
+        }
+
+        public void TDelete(JobApplication t)
+        {
+            _jobApplicationDal.Delete(t);
+        }
+
+        public JobApplication TGetByID(int id)
+        {
+            return _jobApplicationDal.GetByID(id);
+        }
+
+        public void TUpdate(JobApplication t)
+        {
+            _jobApplicationDal.Update(t);
+        }
+    }
+}
