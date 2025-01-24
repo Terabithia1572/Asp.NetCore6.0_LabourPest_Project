11dfae61eec5d42a353af74d9572575ebd855900
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-24 22:13:07 +0300

    Log Controller Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/LogController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class LogController : Controller
+    {
+        LogManager logManager = new LogManager(new EfLogRepository());
+        public IActionResult LogList()
+        {
+            var values = logManager.GetAll()
+                         .OrderByDescending(x => x.Id) // Listeyi Id'ye göre tersten sırala (en son eklenen en üstte olacak)
+                         .Take(10)                      // Son 10 öğeyi al
+                         .ToList();
+            return View(values);
+        }
+        public IActionResult LogListAll()
+        {
+            var values = logManager.GetAll();
+
+            return View(values);
+        }
+    }
+}
