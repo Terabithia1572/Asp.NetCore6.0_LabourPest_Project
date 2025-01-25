40098fa6c5b31904462ba15bbfbdadf003a7ecf1
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-01-25 18:08:41 +0300

    Photos View Component Page Theme Updated

--- Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/PhotosViewComponentPartial.cs
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/PhotosViewComponentPartial.cs
@@ -9,8 +9,21 @@ namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.MainLayout
 		ImageManager _imageManager = new(new EfImagesRepository());
 		public IViewComponentResult Invoke()
 		{
-			var values=_imageManager.GetAll();
-            return View(values);
-		}
+            var images = _imageManager.GetAll()
+            .OrderByDescending(x => x.ImageID) // En son eklenenler önce gelsin
+            .Take(4) // Son 4 veriyi al
+            .ToList();
+
+            // Özel sıralama işlemi
+            var sortedImages = images
+                .OrderBy(x =>
+                    x.ImageClass == "col-md-6 corporate" ? 0 : // 1. sırada corporate
+                    x.ImageClass == "col-md-6 entertainment innovations" ? 1 : // 2. sırada entertainment
+                    2 // 3. ve 4. sırada col-md-3
+                )
+                .ToList();
+
+            return View(sortedImages);
+        }
 	}
 }
