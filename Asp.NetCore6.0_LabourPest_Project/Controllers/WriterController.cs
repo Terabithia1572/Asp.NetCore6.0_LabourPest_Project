76096de8bd96860e83adf3450b9f772ff0f55122
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-01 22:48:36 +0300

    Writer Controller Updated

--- Asp.NetCore6.0_LabourPest_Project/Controllers/WriterController.cs
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/WriterController.cs
@@ -1,13 +1,19 @@
-﻿using DataAccessLayer.Concrete;
+﻿using Asp.NetCore6._0_LabourPest_Project.Models;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Asp.NetCore6._0_LabourPest_Project.Controllers
 {
     [AllowAnonymous]
     public class WriterController : Controller
     {
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
         Context context = new Context();
         public IActionResult Index()
         {
@@ -15,7 +21,23 @@ namespace Asp.NetCore6._0_LabourPest_Project.Controllers
         }
        public IActionResult Test()
         {
-            return View();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            int writerId = Convert.ToInt32(userId);
+
+            // Giriş yapan kullanıcının bilgilerini getiriyoruz.
+            Writer writer = writerManager.TGetByID(writerId);
+
+            // Giriş yapan kullanıcının son 3 blogunu getiriyoruz.
+            var blogs = blogManager.GetRecentBlogsByWriter(writerId, 3);
+
+            // ViewModel'e dolduruyoruz.
+            ProfileViewModel viewModel = new ProfileViewModel
+            {
+                Writer = writer,
+                Blogs = blogs
+            };
+
+            return View(viewModel);
         }
     }
 }
