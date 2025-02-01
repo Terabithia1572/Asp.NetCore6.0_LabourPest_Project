bd02ae8d676ec7dbbe7a6a7a07833818fd649188
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-01 18:51:41 +0300

    New Migrations Added

---
+++ DataAccessLayer/Migrations/20250201154604_mig_Writers_Blogs_BlogComments_BlogCategory_Include.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_Writers_Blogs_BlogComments_BlogCategory_Include : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
