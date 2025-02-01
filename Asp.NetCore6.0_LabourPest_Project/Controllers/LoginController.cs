4a1d426164b919e46cd9a7e7a7f355ba85ce4d63
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-01 13:54:11 +0300

    Login Controller Process Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignIN()
+        {
+            return View();
+        }
+        public IActionResult SignUP()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult RegisterCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterCustomer(Writer writer)
+        {
+            writer.WriterStatus = true;
+            writer.WriterAbout = "Müşteri";
+            writerManager.TAdd(writer);
+            return RedirectToAction("Deneme","Home");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                // Klasör yolunu tanımla
+                string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "labourpestcustomer", "registerCustomer");
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                // Dosya ismini oluştur
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                // Dosya yolunu oluştur
+                string filePath = Path.Combine(folderPath, fileName);
+
+                // Dosyayı kaydet
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                // Dosya yolunu döndür
+                string relativePath = $"/labourpestcustomer/registerCustomer/{fileName}";
+                return Json(new { filePath = relativePath });
+            }
+
+            return BadRequest("Dosya yüklenemedi!");
+        }
+    }
+}
