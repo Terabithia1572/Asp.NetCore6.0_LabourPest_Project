93902d6378eaed83d5a07683abe197f5e0cc2b84
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-01 16:02:23 +0300

    Writer Dashboard Added + Updated

---
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/WriterDashboardLayout/_WriterCssViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.WriterDashboardLayout
+{
+    public class _WriterCssViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
