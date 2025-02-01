935489fc96725f83b5b863ff7f78baed69c3911d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-01 18:49:52 +0300

    View Models Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Models/ViewModels.cs
@@ -0,0 +1,8 @@
+﻿namespace Asp.NetCore6._0_LabourPest_Project.Models
+{
+    public class ProfileViewModel
+    {
+        public EntityLayer.Concrete.Writer Writer { get; set; }
+        public IEnumerable<EntityLayer.Concrete.Blog> Blogs { get; set; }
+    }
+}
