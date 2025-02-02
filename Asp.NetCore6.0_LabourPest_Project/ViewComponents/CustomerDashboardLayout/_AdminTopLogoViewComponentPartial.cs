204b79ce0803e13fab870f021a0c6d562d979caa
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-02 22:20:35 +0300

    Admin Top Logo Updated + New theme Added

--- Asp.NetCore6.0_LabourPest_Project/ViewComponents/CustomerDashboardLayout/_AdminTopLogoViewComponentPartial.cs
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/CustomerDashboardLayout/_AdminTopLogoViewComponentPartial.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.CustomerDashboardLayout
 {
@@ -6,7 +10,19 @@ namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.CustomerDashboardLay
     {
         public IViewComponentResult Invoke()
         {
-            return View();
+            var userIdString = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userIdString))
+            {
+                // Giriş yapılmamışsa veya id bulunamazsa boş model gönderilebilir.
+                return View(null);
+            }
+            int writerId = Convert.ToInt32(userIdString);
+
+            // WriterManager üzerinden ilgili yazar bilgilerini çekiyoruz.
+            WriterManager writerManager = new WriterManager(new EfWriterRepository());
+            Writer writer = writerManager.TGetByID(writerId);
+
+            return View(writer);
         }
     }
 }
