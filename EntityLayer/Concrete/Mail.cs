e3076a1d80660ac229ac2bfc055273f7fd3d273f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-02 13:17:22 +0300

    Mail Process Added

--- EntityLayer/Concrete/Mail.cs
+++ EntityLayer/Concrete/Mail.cs
@@ -9,13 +9,13 @@ namespace EntityLayer.Concrete
 {
     public class Mail
     {
-        [Key]
         public int MailID { get; set; }
+        public string ReceiverMail { get; set; }
+        public string SenderMail { get; set; } // Gönderen mail adresi
         public string MailName { get; set; }
         public string MailSurname { get; set; }
         public string MailTitle { get; set; }
         public string MailContent { get; set; }
-        public string ReceiverMail { get; set; }
         public DateTime MailDate { get; set; }
     }
 }
