579ed76e9f2482a368c2b4e0a716ffc2fc0c2727
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-02 22:18:30 +0300

    CommentController Updated

--- Asp.NetCore6.0_LabourPest_Project/Controllers/CommentController.cs
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/CommentController.cs
@@ -1,10 +1,12 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore6._0_LabourPest_Project.Controllers
 {
+    [AllowAnonymous]
     public class CommentController : Controller
     {
         CommentManager commentManager = new CommentManager(new EfCommentRepository());
