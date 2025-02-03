de7938bd6aa3a91d8b796976efd218dde6350ef9
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-03 23:33:05 +0300

    new Migration Added for IP Address

---
+++ DataAccessLayer/Migrations/20250202133546_mig_Log_IpAdress_added.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_Log_IpAdress_added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
