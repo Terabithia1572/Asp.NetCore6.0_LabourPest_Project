5ae312e36d9520b9f1f32b2e38321fd91c0924e6
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-03 23:29:46 +0300

    FileListViewModel Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Models/FileListViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Asp.NetCore6._0_LabourPest_Project.Models
+{
+    // Models/FileListViewModel.cs
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class FileListViewModel
+    {
+        public List<FileItem> AdminFiles { get; set; } = new List<FileItem>();
+        public List<FileItem> MusteriFiles { get; set; } = new List<FileItem>();
+
+        /// <summary>
+        /// Tüm dosyalar (Admin + Müşteri)
+        /// </summary>
+        public List<FileItem> AllFiles
+        {
+            get { return AdminFiles.Concat(MusteriFiles).ToList(); }
+        }
+
+        /// <summary>
+        /// Son eklenen 8 dosya (her iki kategoriden)
+        /// </summary>
+        public List<FileItem> RecentFiles { get; set; } = new List<FileItem>();
+    }
+
+}
