b02c5b57994fec459de3da32f7eecd22696fe218
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-05 22:07:19 +0300

    Mail With Sender View Model Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Models/MailWithSenderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using EntityLayer.Concrete;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Models
+{
+    public class MailWithSenderViewModel
+    {
+        public Mail Mail { get; set; }
+        public string SenderImage { get; set; }
+    }
+}
