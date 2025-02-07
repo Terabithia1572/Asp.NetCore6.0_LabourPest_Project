cb65decf5d9f1c7ccec2f6102a1972707f027972
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-07 20:41:22 +0300

    FAQ Controller Added

---
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/FAQsController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.Controllers
+{
+    public class FAQsController : Controller
+    {
+        FAQManager faqManager= new FAQManager(new EfFAQRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FAQList()
+        {
+            var values=faqManager.GetAll();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddFAQ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddFAQ(FAQ faq)
+        {
+            faqManager.TAdd(faq);
+            return RedirectToAction("FAQList","FAQ");
+        }
+
+    }
+}
