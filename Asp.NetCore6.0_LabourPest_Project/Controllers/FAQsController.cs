d19d61b797eba553469835405c982d8bb11db77a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-10 23:38:51 +0300

    FAQs Controller CRUD Process Added

--- Asp.NetCore6.0_LabourPest_Project/Controllers/FAQsController.cs
+++ Asp.NetCore6.0_LabourPest_Project/Controllers/FAQsController.cs
@@ -1,12 +1,15 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore6._0_LabourPest_Project.Controllers
 {
+    [Authorize]
     public class FAQsController : Controller
     {
+        
         FAQManager faqManager= new FAQManager(new EfFAQRepository());
         public IActionResult Index()
         {
@@ -28,6 +31,25 @@ namespace Asp.NetCore6._0_LabourPest_Project.Controllers
             faqManager.TAdd(faq);
             return RedirectToAction("FAQList","FAQ");
         }
+        [HttpGet]
+        public IActionResult UpdateFAQ(int id)
+        {
+            var faqValue = faqManager.TGetByID(id);
+            return View(faqValue);
+        }
+        [HttpPost]
+        public IActionResult UpdateFAQ(FAQ faq)
+        {
+            faqManager.TUpdate(faq);
+            return RedirectToAction("FAQList", "FAQ");
+        }
+        public IActionResult DeleteFAQ(int id)
+        {
+            var faqValue = faqManager.TGetByID(id);
+            faqManager.TDelete(faqValue);
+            return RedirectToAction("FAQList", "FAQ");
+        }
+
 
     }
 }
