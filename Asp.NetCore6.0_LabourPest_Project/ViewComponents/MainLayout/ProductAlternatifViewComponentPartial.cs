7ac1b42f8717bdeffe3a0a3ac696b3e43e8641ee
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-02-16 15:59:51 +0300

    Alternative Product Page Added

---
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/MainLayout/ProductAlternatifViewComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.MainLayout
+{
+    public class ProductAlternatifViewComponentPartial:ViewComponent
+    {
+        ProductManager productManager = new(new EfProductRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values=productManager.GetListWithProductCategory()
+                .OrderByDescending(x=>x.ProductID)
+                .Take(6)
+                .ToList();
+            return View(values);
+        }
+    }
+}
