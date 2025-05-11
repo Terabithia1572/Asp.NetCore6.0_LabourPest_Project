0442533c30b0f2fb79083067b521cf99ae447f2e
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-05-11 17:43:24 +0300

    BlogDetailView Model Updated

---
+++ Asp.NetCore6.0_LabourPest_Project/Models/BlogDetailViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Asp.NetCore6._0_LabourPest_Project.Models
+{
+    public class BlogDetailViewModel
+    {
+        public EntityLayer.Concrete.Blog Blog { get; set; }
+        public EntityLayer.Concrete.Writer Writer { get; set; }
+        public List<EntityLayer.Concrete.BlogComment> Comments { get; set; }
+    }
+}
