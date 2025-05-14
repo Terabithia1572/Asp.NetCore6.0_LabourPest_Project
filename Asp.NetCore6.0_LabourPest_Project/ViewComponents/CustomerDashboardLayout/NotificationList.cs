8636776b50cfa7b7eb959950e6497d1c10af6863
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-05-14 22:55:25 +0300

    NotificationList Process Added

---
+++ Asp.NetCore6.0_LabourPest_Project/ViewComponents/CustomerDashboardLayout/NotificationList.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Asp.NetCore6._0_LabourPest_Project.ViewComponents.CustomerDashboardLayout
+{
+    public class NotificationList : ViewComponent
+    {
+        NotificationManager notificationManager = new NotificationManager(new EfNotificationRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var userId = UserClaimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+            int writerId = Convert.ToInt32(userId);
+
+            var values = notificationManager.GetListByWriter(writerId)
+                                            .Where(x => x.NotificationStatus == false)
+                                            .Take(5)
+                                            .ToList();
+
+            return View(values);
+        }
+
+    }
+}
