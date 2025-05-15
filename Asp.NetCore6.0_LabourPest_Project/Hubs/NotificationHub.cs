18cbaa59a4f43d23c735af4fe0b9799e2cf0fba7
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-05-15 20:23:17 +0300

    Notification Hub SignalR added

---
+++ Asp.NetCore6.0_LabourPest_Project/Hubs/NotificationHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Asp.NetCore6._0_LabourPest_Project.Hubs;
+
+
+namespace Asp.NetCore6._0_LabourPest_Project.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        // SignalR üzerinden mesaj iletimi yapılacak yer
+    }
+}
