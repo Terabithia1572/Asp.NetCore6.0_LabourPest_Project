00da61717a5a3913176c195e617c15c39a58c363
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-05-16 01:11:40 +0300

    notification js file added

---
+++ Asp.NetCore6.0_LabourPest_Project/wwwroot/js/notification.js
@@ -0,0 +1,20 @@
+﻿let connection = new signalR.HubConnectionBuilder()
+    .withUrl("/notificationhub")
+    .build();
+
+function loadNotifications() {
+    $.get('/Notification/GetLatestNotifications', function (data) {
+        $('#notificationList').html(data);
+    });
+}
+
+connection.on("ReceiveNotification", function () {
+    loadNotifications();
+});
+
+connection.start().then(function () {
+    loadNotifications();
+    console.log("✅ SignalR bağlantısı kuruldu.");
+}).catch(function (err) {
+    console.error("❌ SignalR bağlantı hatası:", err.toString());
+});
