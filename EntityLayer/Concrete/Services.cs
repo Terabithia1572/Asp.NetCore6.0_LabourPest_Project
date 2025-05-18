cf1d5758249f20d619342bc34f2253165301a0b8
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-05-18 03:28:06 +0300

    new Images Added + Services View Component Updated

--- EntityLayer/Concrete/Services.cs
+++ EntityLayer/Concrete/Services.cs
@@ -12,7 +12,11 @@ namespace EntityLayer.Concrete
         [Key]
         public int ServicesID { get; set; }
         public string ServicesTitle { get; set; }
+        public string? ServicesSlug { get; set; }
+
         public string ServicesDescription { get; set; }
+        public string? ServicesLongDescription { get; set; }
+
         public string ServicesImageURL { get; set; }
         public bool ServicesStatus { get; set; }
     }
